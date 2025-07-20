720a4c4cf46344d33180a1018226077d5f202d2e
PastaLaPate <75879845+PastaLaPate@users.noreply.github.com>
2025-07-20 23:46:24 +0200

    Added FacableBlock

---
+++ src/main/java/com/github/pastalapate/torcherino_balanced/blocks/FacableBlock.java
@@ -0,0 +1,30 @@
+package com.github.pastalapate.torcherino_balanced.blocks;
+
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import org.jetbrains.annotations.Nullable;
+
+public class FacableBlock extends Block {
+
+    public static final DirectionProperty FACING = BlockStateProperties.FACING;
+
+    public FacableBlock(Properties properties) {
+        super(properties);
+        registerDefaultState(getStateDefinition().any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public @Nullable BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+}
